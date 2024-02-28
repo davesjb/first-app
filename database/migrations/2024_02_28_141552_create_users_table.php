c1a698fac463f50719e4c7a14425a0a311c1e977
Dave New Mac <davesjb@gmail.com>
2024-02-28 15:07:01 +0000

    Up function, rollback, refresh, fresh, status

---
+++ database/migrations/2024_02_28_141552_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration; //Send create migration
+use Illuminate\Database\Schema\Blueprint; // Tables 
+use Illuminate\Support\Facades\Schema; //Design of table 
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("username2", 32);
+            $table->string("password", 255);
+            $table->string("email", 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
--- database/migrations/2024_02_28_142738_create_products_table.php
+++ database/migrations/2024_02_28_142738_create_products_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string("product_name", 32);
+            $table->string("product_description", 255);
+            $table->float("product_price", 4, 2);
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
